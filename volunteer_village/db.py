c1a9ffef10516d5ba0297a4cc4b2db1dd81b19c7
parrothacker1 <rajeevgoutham1@protonmail.com>
2024-03-23 06:00:11 +0530

    Fixed templated

--- volunteer_village/db.py
+++ volunteer_village/db.py
@@ -3,6 +3,7 @@ from volunteer_village.config import config
 from sqlalchemy import create_engine,Column,String,Integer,insert,select,text
 from sqlalchemy.engine import URL
 from sqlalchemy.orm import declarative_base,sessionmaker
+from sqlalchemy.dialects.postgresql import ARRAY
 
 from hashlib import sha256
 from pydantic import BaseModel
@@ -15,10 +16,25 @@ connection=engine.connect()
 Session=sessionmaker(bind=engine)
 session=Session()
 
+class TaskInput(BaseModel):
+    task_name:str
+    task_description:str
+    city:str
+    addess:str
+    start_date:str
+    end_date:str
+    skills:list(str)
+    volunteers:list(str)|None
+
+class ProfileUpdate(BaseModel):
+    email:str
+    name:str
+    state:str
+    city:str
+    phone_num:str
+
 class Signup(BaseModel):
-    name:str|None
-    first_name:str|None
-    last_name:str|None
+    name:str
     email:str
     phone_num:str
     state:str|None
@@ -30,40 +46,110 @@ class Volunteers(Base):
     id=Column(Integer,primary_key=True)
     email=Column(String,nullable=False,unique=True)
     password_hash=Column(String(70),nullable=False,unique=True)
-    first_name=Column(String)
-    last_name=Column(String)
+    name=Column(String)
     phone_num=Column(String)
     state=Column(String)
     city=Column(String)
-    skills=Column(Integer,nullable=False)
 
 class Organisers(Base):
     __tablename__="organisers"
     id=Column(Integer,primary_key=True)
     email=Column(String,nullable=False,unique=True)
-    password_hash=Column(String,nullable=False,unique=True)
-    org_name=Column(String)
+    password_hash=Column(String(70),nullable=False,unique=True)
+    name=Column(String)
     phone_num=Column(String)
-    state=Column(String)
-    city=Column(String)
 
 class Tasks(Base):
     __tablename__="tasks"
     id=Column(Integer,primary_key=True)
-    required_skills=Column(Integer)
+    skills=Column(ARRAY(String))
     task_name=Column(String)
+    organiser=Column(String)
     task_description=Column(String)
     address=Column(String)
-    start_date=Column(String) #TODO: Here add start date
-    end_date=Column(String) #TODO: Here add end date
-    state=Column(String)
+    start_date=Column(String)
+    end_date=Column(String)
     city=Column(String)
+    volunteers=Column(ARRAY(String))
 
 #class Feedback(Base):
 #    __tablename__="feedbacks"
 
+# Tasks
+def full_tasks_list():
+    result=select(Tasks)
+    return result
+
+def tasks_list(email:str,user:str):
+    if (user=="volunteer"):
+        user=select(Volunteers.name).where(Volunteers.email==email).first()
+        tasks=select(Tasks).where(user[0] in Tasks.volunteers)
+        result=tasks
+    elif (user=="organiser"):
+        user=select(Organisers.name).where(Organisers.email==email).first()
+        tasks=select(Tasks).where(organiser==user[0])
+        result=tasks
+    else:
+        return None
+    return result
+
+def task_add(email:str,tasks_input:TaskInput,user:str):
+    user=select(Organisers.name).where(Organisers.email==email).first()
+    if (tasks_list(email,user).count()>0 && user=="organiser"):
+        content={"organiser":user[0],"skills":tasks_input.skills,"city":tasks_input.city,"start_date":tasks_input.start_date,"end_date":tasks_input.end_date,"task_name":tasks_input.task_name,"task_description":tasks_input.task_description,"address":tasks_input.address,"volunteers":[]}
+        task_cr=Tasks(**content)
+        session.add(task_cr)
+        session.commit()
+        return True
+    else:
+        return False
+
+def task_edit(id:int,update:TasksInput):
+    obj_to_update = session.query(Tasks).filter(Tasks.id == id).first()
+    if obj_to_update:
+        obj_to_update.task_name=update.task_name
+        obj_to_update.city=update.city
+        obj_to_update.address=update.address
+        obj_to_update.task_description=update.task_description
+        obj_to_update.start_date=update.start_date
+        obj_to_update.end_date=update.end_date
+        obj_to_update.skills=update.skills
+        obj_to_update.volunteers=update.volunteers
+        session.commit()
+        return True
+    else:
+        return False
+
+
+def task_delete(id:int):
+    output=None
+    delete_query=session.query(Tasks).filter(Tasks.id==id).first()
+    if (delete_query):
+        session.delete(delete_query)
+        session.commit()
+        return True
+    else:
+        return False
 
 # Users
+
+def update_user(update:ProfileUpdate,user:str)-> bool:
+    if (user=="volunteer"):
+        obj_to_update = session.query(Volunteers).filter(Volunteers.email == update.email).first()
+    elif (user=="organiser"):
+        obj_to_update = session.query(Organisers).filter(Organisers.email == update.email).first()
+    else:
+        return None
+    if obj_to_update:
+        obj_to_update.name=update.name
+        obj_to_update.city=update.city
+        obj_to_update.phone_num=update.phone_num
+        obj_to_update.state=update.state
+        session.commit()
+        return True
+    else:
+        return False
+
 def verify_user(email:str,password:str,user:str) -> bool:
     if (user=="volunteer"):
         output=session.execute(select(Volunteers.password_hash).where(Volunteers.email==email)).first()
@@ -71,17 +157,38 @@ def verify_user(email:str,password:str,user:str) -> bool:
         output=session.execute(select(Organisers.password_hash).where(Organisers.email==email)).first()
     else:
         return False
-    pass_hash=sha256(password.encode('utf-8')).hexdigest()
+    pass_hash=sha256(password.encode('utf-8')).hexdigest())
     return output[0]==pass_hash
 
 def create_user(signup:Signup,user:str):
     if (user=="volunteer"):
-        user_cr=insert()
+        content={"email":signup.email,"password_hash":sha256(signup.password.encode('utf-8').hexdigest()),"name":signup.name,"phone_num":signup.phone_num,"state":signup.state,"city":signup.city}
+        if (not user_exists(signup.email,user)):
+            user_cr=Volunteers(**content)
+        else:
+            return False
     elif (user=="organiser"):
-
+        content={"email":signup.email,"password_hash":sha256(signup.password.encode('utf-8').hexdigest()),"name":signup.name,"phone_num":signup.phone_num}
+        if (not user_exisrs(signup.email,user)):
+            user_cr=Organisers(**content)
+        else:
+            return False
     else:
         return None
+    session.add(user_cr)
+    session.commit()
+    return True
 
+def get_user(email:str,user:str):
+    if (user=="volunteer"):
+        columns_to_query = [getattr(Volunteers, column.name) for column in Volunteers.__table__.columns if column.name != 'password_hash']
+        user_get=select(*columns_to_query).where(Volunteers.email==email)
+    elif (user=="organiser"):
+        columns_to_query = [getattr(Organisers, column.name) for column in Organisers.__table__.columns if column.name != 'password_hash']
+        user_get=select(*columns_to_query).where(Organisers.email==email)
+    else:
+        return None
+    return user_get
 
 def user_exists(email:str,user:str) -> bool:
     if (user=="volunteer"):
@@ -95,7 +202,24 @@ def user_exists(email:str,user:str) -> bool:
 def get_user_data(email:str,user:str):
     output=None
     if (user=="volunteer"):
-        output=session.execute(select(Volunteers).where(Volunteers.email==email))
+        columns_to_query = [getattr(Volunteers, column.name) for column in Volunteers.__table__.columns if column.name != 'password_hash']
+        output=session.execute(select(*columns_to_query).where(Volunteers.email==email))
     elif (user=="organiser"):
-        output=session.execute(select(Organisers).where(Organisers.email==email))
+        columns_to_query = [getattr(Organisers, column.name) for column in Organisers.__table__.columns if column.name != 'password_hash']
+        output=session.execute(select(*columns_to_query).where(Organisers.email==email))
     return output
+
+def delete_user(email:str,user:str) -> bool:
+    output=None
+    if (user=="volunteer"):
+        delete_query=session.query(Volunteers).filter(Volunteers.email==email).first()
+    elif (user=="organiser"):
+        delete_query=session.query(Organisers).filter(Organisers.email==email).first()
+    else:
+        return output
+    if (delete_query):
+        session.delete(delete_query)
+        session.commit()
+        return True
+    else:
+        return False
--- volunteer_village/main.py
+++ volunteer_village/main.py
@@ -1,10 +1,8 @@
-# TODO: CRUD user data 
-# TODO: Login(Done) and signup(done)
-# TODO: Tasks (Add delete edit)
-
 from fastapi import FastAPI,Response,status,Request 
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from volunteer_village.config import config
 from volunteer_village import db
@@ -12,7 +10,10 @@ from volunteer_village import db
 import jwt as JWT
 import datetime
 
-app=FastAPI(redoc_url=False)
+app=FastAPI(docs_url=False,redoc_url=False)
+BASE_DIR=Path(__file__).resolve().parent
+app.mount("/static",StaticFiles(directory=str(Path(BASE_DIR,'static'))),name='static')
+templates=Jinja2Templates(directory=str(Path(BASE_DIR,'templates')))
 
 class Login(BaseModel):
     email:str
@@ -35,9 +36,12 @@ async def api_volunteer_login(login:Login,user:str):
 
 @app.post("/api/{user}/signup")
 async def api_signup(signup:db.Signup,user:str):
-    user_exists=db.create_user(signup,user)
+    if (user=="volunteer" && state && city) or (user=="organiser"): 
+        user_exists=db.create_user(signup,user)
+    else:
+        return JSONResponse(content="wrong_format",status_code=status.HTTP_400_BAD_REQUEST)
     response={"content":"already_exists","status_code":HTTP_409_CONFLICT}
-    if(not user_exists):
+    if(user_exists):
         payload={"content":signup.email,"user":user}
         payload["exp"]=datetime.datetime.now()+datetime.timedelta(days=120)
         access_jwt=JWT.encode(payload,config['access_token'],algorithm="HS512")
@@ -53,25 +57,43 @@ async def api_getinfo(request:Request):
     response={"content":"wrong_jwt","status_code":status.HTTP_401_UNAUTHORIZED}
     try:
         jwt_decoded=JWT.decode(token,config['refresh_token'],algorithms=["HS512"])
-        if(db.user_exists(jwt_decoded["email"],jwt_decoded["user"])):
-            # get info of the user
-            response["content"]="give user info"
-            response["status_code"]=status.HTTP_200_OK
+        try:
+            if(db.user_exists(jwt_decoded["email"],jwt_decoded["user"])): 
+                response["content"]=str(db.get_user(jwt_decoded['email'],jwt_decoded['email']))
+                response["status_code"]=status.HTTP_200_OK
+            else:
+                response["content"]="user_not_exists"
+                response["status_code"]=status.HTTP_404_NOT_FOUND
+        except:
+            response["content"]="sql_error"
+            response["status_code"]=status.HTTP_500_INTERNAL_SERVER_ERROR
     except JWT.exceptions.ExpiredSignatureError():
         response["content"]="jwt_expired"
+        response["status_code"]=status.HTTP_426_UPGRADE_REQUIRED
     return JSONResponse(**response)
 
 @app.put("/api/user")
-async def api_putinfo():
+async def api_putinfo(user_update:db.ProfileUpdate):
     token=request.headers["Authorization"].split(" ")[1]
     response={"content":"wrong_jwt","status_code":status.HTTP_401_UNAUTHORIZED}
     try:
         jwt_decoded=JWT.decode(token,config['refresh_token'],algorithms=["HS512"])
-        if (db.user_exists(jwt_decoded["email"],jwt_decoded["user"])):
-            response["content"]="update"
-            response["status_code"]=status.HTTP_200_OK
+        try:
+            if (db.user_exists(jwt_decoded["email"],jwt_decoded["user"])):
+                response["content"]="success" if db.update_user(user_update,jwt_decoded["user"]) else "failed"
+                response["status_code"]=status.HTTP_200_OK
+            else:
+                response["content"]="user_not_exists"
+                response["status_code"]=status.HTTP_404_NOT_FOUND
+        except:
+            response["content"]="sql_error"
+            response["status_code"]=status.HTTP_500_INTERNAL_SERVER_ERROR
+        else:
+            response["content"]="user_not_found"
+            response["status_code"]=status.HTTP_404_NOT_FOUND
     except JWT.exceptions.ExpiredSignatureError():
         response["content"]="jwt_expired"
+        response["status_code"]=status.HTTP_426_UPGRADE_REQUIRED
     return JSONResponse(**response)
 
 @app.delete("/api/user")
@@ -80,11 +102,19 @@ async def api_deleteinfo():
     response={"content":"wrong_jwt","status_code":status.HTTP_401_UNAUTHORIZED}
     try:
         jwt_decoded=JWT.decode(token,config['refresh_token'],algorithms=["HS512"])
-        if (db.user_exists(jwt_decoded["email"],jwt_decoded["user"])):
-            response["content"]="delete the user"
-            response["status_code"]=status.HTTP_200_OK
+        try:
+            if (db.user_exists(jwt_decoded["email"],jwt_decoded["user"])):
+                response["content"]="success" if db.delete_user(jwt_decoded['email'],jwt_decoded['user']) else "failed"
+                response["status_code"]=status.HTTP_200_OK
+            else:
+                response["content"]="user_not_exists"
+                response["status_code"]=status.HTTP_404_NOT_FOUND
+        except:
+            response["content"]="sql_error"
+            response["status_code"]=status.HTTP_500_INTERNAL_SERVER_ERROR
     except JWT.exceptions.ExpiredSignatureError():
         response["content"]="jwt_expired"
+        response["status_code"]=status.HTTP_426_UPGRADE_REQUIRED
     return JSONResponse(**response)
 
 @app.post("/api/token/refresh_token")
@@ -94,47 +124,121 @@ async def api_refresh_token(request:Request):
     try:
         jwt_decoded=JWT.decode(access_token,config['access_token'],algorithms=["HS512"])
         payload=jwt_decoded
-        payload["exp"]=datetime.datetime.now()+datetime.timedelta(minutes=15)
-        refresh_jwt=JWT.encode(payload,cconfig['refresh_token'],algorithm="HS512")
-        response["content"]=str({"refresh_token":refresh_jwt})
-        response["status_code"]=status.HTTP_200_OK
+        if (db.user_exists(payload['email'],payload['user'])):
+            payload["exp"]=datetime.datetime.now()+datetime.timedelta(minutes=15)
+            refresh_jwt=JWT.encode(payload,config['refresh_token'],algorithm="HS512")
+            response["content"]=str({"refresh_token":refresh_jwt})
+            response["status_code"]=status.HTTP_200_OK
+        else:
+            response["content"]="user_not_found"
     except JWT.exceptions.ExpiredSignatureError():
         response["content"]="expired_access_token"
         response["status_code"]=status.HTTP_426_UPGRADE_REQUIRED
     return JSONResponse(**response)
 
 @app.get("/api/tasks")
-async def api_tasks_get():
-    #TODO: Organisation name
-    #TODO: Task name
-    #TODO: Address
-    #TODO: City and state 
-    #TODO: Skills requirements
-    return Response("get tasks list")
+async def api_tasks_get(request:Request):
+    token=request.headers["Authorization"].split(" ")[1]
+    response={"content":"not_authorized","status_code":status.HTTP_401_UNAUTHORIZED}
+    try:
+        jwt_decoded=JWT.decode(token,config['refresh_token'],algorithms=["HS512"])
+        try:
+            tasks_list=db.tasks_list(jwt_decoded['email'],jwt_decoded['user'])
+            response["content"]=str(tasks_list)
+            response["status_code"]=status.HTTP_200_OK
+        except:
+            response["content"]="sql_error"
+            response["status_code"]=status.HTTP_500_INTERNAL_SERVER_ERROR
+    except JWT.exceptions.ExpiredSignatureError():
+        response["content"]="expired_refresh_token"
+        response["status_code"]=status.HTTP_426_UPGRADE_REQUIRED
+    return JSONResponse(**response)
 
+@app.get("/api/tasks/list")
+async def api_tasks_get_list():
+    response={'content':'sql_error',"status_code":status.HTTP_500_INTERNAL_SERVER_ERROR}
+    try:
+        response["content"]=str(db.full_tasks_list())
+        response["status_code"]=status.HTTP_200_OK
+    except:
+        pass
+    return JSONResponse(**response)
 
 @app.post("/api/tasks")
-async def api_tasks_post():
-    return Response("post tasks lists")
+async def api_tasks_post(task_input:db.TaskInput):
+    token=request.headers["Authorization"].split(" ")[1]
+    response={"content":"not_authorized","status_code":status.HTTP_401_UNAUTHORIZED}
+    try:
+        jwt_decoded=JWT.decode(token,config['refresh_token'],algorithms=["HS512"])
+        try:
+            tasks_list=db.tasks_add(jwt_decoded['email'],task_input,jwt_decoded['user'])
+            response["content"]=str(tasks_list)
+            response["status_code"]=status.HTTP_200_OK
+        except:
+            response["content"]="sql_error"
+            response["status_code"]=status.HTTP_500_INTERNAL_SERVER_ERROR
+    except JWT.exceptions.ExpiredSignatureError():
+        response["content"]="expired_refresh_token"
+        response["status_code"]=status.HTTP_426_UPGRADE_REQUIRED
+    return JSONResponse(**response)
 
-@app.put("/api/tasks")
-async def api_tasks_put():
-    return Response("put tasks list")
+@app.put("/api/tasks/{id}")
+async def api_tasks_put(id:int,update:db.TaskInput):
+    token=request.headers["Authorization"].split(" ")[1]
+    response={"content":"not_authorized","status_code":status.HTTP_401_UNAUTHORIZED}
+    try:
+        jwt_decoded=JWT.decode(token,config['refresh_token'],algorithms=["HS512"])
+        try:
+            response["content"]="success" if db.task_edit(id,update) else "failed"
+            response["status_code"]=status.HTTP_200_OK
+        except:
+            response["content"]="sql_error"
+            response["status_code"]=status.HTTP_500_INTERNAL_SERVER_ERROR
+    except JWT.exceptions.ExpiredSignatureError():
+        response["content"]="expired_refresh_token"
+        response["status_code"]=status.HTTP_426_UPGRADE_REQUIRED
+    return JSONResponse(**response)
 
-@app.delete("/api/tasks")
-async def api_tasks_del():
-    return Response("delete tasks list")
+@app.delete("/api/tasks/{id}")
+async def api_tasks_del(id:int):
+    token=request.headers["Authorization"].split(" ")[1]
+    response={"content":"not_authorized","status_code":status.HTTP_401_UNAUTHORIZED}
+    try:
+        jwt_decoded=JWT.decode(token,config['refresh_token'],algorithms=["HS512"])
+        try:
+            response["content"]="success" if db.tasks_del(id) else "failed"
+            response["status_code"]=status.HTTP_200_OK
+        except:
+            response["content"]="sql_error"
+            response["status_code"]=status.HTTP_500_INTERNAL_SERVER_ERROR
+    except JWT.exceptions.ExpiredSignatureError():
+        response["content"]="expired_refresh_token"
+        response["status_code"]=status.HTTP_426_UPGRADE_REQUIRED
+    return JSONResponse(**response)
 
 # Webpage Endpoints
+
 @app.get("/")
 async def home():
-    return Response("home")
+    response=templates.TemplateResponse('index.html',{'request':request})
+    return response
 
-@app.get("/access-account")
-async def access_account():
-    return Response("login or signup")
+@app.get("/access-account/login")
+async def access_account_login():
+    response=templates.TemplateResponse('login.html',{'request':request})
+    return response
+
+@app.get("/access-account/signup")
+async def access_account_signup():
+    response=templates.TemplateResponse('signup2.html',{'request':request})
+    return response
 
 @app.get("/tasks")
 async def tasks():
-    return Response("tasks")
+    response=templates.TemplateResponse('login.html',{'request':request})
+    return response
 
+@app.get("/about")
+async def about():
+    response=templates.TemplateResponse('About.html',{'request':request})
+    return response
